c5f07bd6bc199f702e0b25a2604f01ef91b0a008
Diana Juarez <dianajuarez@Dianas-MacBook-Pro.local>
2019-02-19 13:35:44 -0600

    updating branches

--- Animals/Animals.UI/Dog.cs
+++ Animals/Animals.UI/Dog.cs
@@ -1,20 +1,51 @@
+using System;
+
 namespace Animals.library
 {
     public class Dog
     {
 
-        public string Noise = "bark!";
+        public string Noise = "bark";
+
+        public string _name {get; set;}
+
+        //use properties(these) instead of getters and setters (best prazctice in C#)
+        private string Name
+        {
+            get{
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string Color { get; } = "brow;";
+
+        public int Id {get; set;} = 0;
 
         public void GoTo(string location)
         {
-            System.Console.WriteLine("Walking to " + location);
+            Console.WriteLine("Walking to " + location);
 
-            System.Console.WriteLine($"Walking to {location}");
+            Console.WriteLine($"Walking to {location}");
         }
 
         public void MakeNoise()
         {
-            System.Console.WriteLine(Noise);
+            Console.WriteLine($"{Noise}!");
+        }
+
+        //setter
+        public void setNoise(string newValue)
+        {
+            if(newValue.Length == null|| newValue.Length == 0)
+            {
+                throw new ArgumentException("Value must not be null or empty");
+
+            }
+            Noise = newValue;
         }
     }
 
